1e998fea8303826dac3ae89f58bc7f3abefdbfc9
yousefyang <75463531+yousefyang@users.noreply.github.com>
2025-03-07 21:22:00 +0800

    Update r codes.R for phylogeny

--- r codes.R
+++ r codes.R
@@ -1,31 +1,9 @@
 ###phylogentic tree based on root cognates (Matisoff 2009)
 library(ape)
-datam<-read.csv("C:\\Users\\Lenovo\\OneDrive\\SYSU\\Research\\Book\\sino-tibetan\\ST cognates.csv")
-datam<-subset(datam, select=2:ncol(datam) )
-rownames(datam)<-c("????????"  ,  "????????",
-                   "????",
-                   "原始??????",
-                   "原始??伽",
-                   "原始????",
-                   "原始??芒",
-                   "原始????",
-                   "????",
-                   "?良?",
-                   "????",
-                   "?瞎藕???","??-羌??","农-????","????支")
+datam<-read.csv("")  #the address is your raw data
+datam<-subset(datam, select=2:ncol(datam) )   #assuming your first variable is names of the cases, else you starts with column 1
+rownames(datam)<-datam$X   #apply to rownames by the first variable in dataset which is labels, or user created vector of labels.
+
 dist_obj=dist(datam, method="euclidean")
-tree<-nj(dist_obj)
-
-plot(tree)
-
-
-#phylo tree of cultural traits
-library(ape)
-datam<-read.csv("C:\\Users\\ii\\Desktop\\genderval.csv", encoding = "UTF-8")
-
-datam2<-subset(datam, select=2:ncol(datam) )
-rownames(datam2)<-datam$X.U.FEFF.sheng
-
-dist_obj=dist(datam2, method="euclidean")
 tree<-nj(dist_obj)
 plot(tree)
