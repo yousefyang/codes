d518048efe7e4e2bd81ba800614c402dc0e2ab4c
yousefyang <75463531+yousefyang@users.noreply.github.com>
2025-03-14 16:52:13 +0800

    Update r codes.R

--- r codes.R
+++ r codes.R
@@ -1,9 +1,9 @@
-###phylogentic tree based on root cognates (Matisoff 2009)
+###phylogentic tree using "ape" package
 library(ape)
 datam<-read.csv("")  #the address is your raw data
 datam<-subset(datam, select=1:ncol(datam) )   #assuming your first variable is names of the cases, else you starts with column 1
 rownames(datam)<-datam$X   #apply to rownames by the first variable in dataset which is labels, or user created vector of labels.
 
-dist_obj=dist(datam, method="euclidean")
+dist_obj=dist(datam, method="euclidean")    #some methods do not allow non-integer
 tree<-nj(dist_obj)
 plot(tree)
