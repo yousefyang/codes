138690e09d35d39ecf26593587fbcd06ef538d58
yousefyang <75463531+yousefyang@users.noreply.github.com>
2025-03-14 16:51:14 +0800

    Update r codes.R

--- r codes.R
+++ r codes.R
@@ -1,7 +1,7 @@
 ###phylogentic tree based on root cognates (Matisoff 2009)
 library(ape)
 datam<-read.csv("")  #the address is your raw data
-datam<-subset(datam, select=2:ncol(datam) )   #assuming your first variable is names of the cases, else you starts with column 1
+datam<-subset(datam, select=1:ncol(datam) )   #assuming your first variable is names of the cases, else you starts with column 1
 rownames(datam)<-datam$X   #apply to rownames by the first variable in dataset which is labels, or user created vector of labels.
 
 dist_obj=dist(datam, method="euclidean")
